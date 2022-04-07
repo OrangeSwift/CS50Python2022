e4710af00db9c8dbc3ff43b52411bb2fd0254edb
OrangeSwift <67804052+OrangeSwift@users.noreply.github.com>
2022-04-07 11:21:30 +0200

    Create playback.py
    Problem 2 of Lab 0 in CS50’s Introduction to Programming with Python 2022

---
+++ playback.py
@@ -0,0 +1,8 @@
+#SDG
+
+# Request User Input
+userInput=input("Input: ")
+# Copy userInput to a new variable and change spaces in string to ...
+userInputChanged=userInput.replace(" ", "...")
+# Print input with changes
+print(userInputChanged)
